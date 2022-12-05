caaceb0b49700afec38227082535b5cddd274127
Cress <cress91@gmail.com>
2022-12-05 12:59:02 +0300

    006

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввел пользователь.
+
+Console.Write("Arount of numbers:");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] a = new int[n];
+
+for(int i=0; i<n; i++)
+{
+    Console.Write($"A[{i + 1}] = ");
+    a[i] = Convert.ToInt32(Console.ReadLine());
+    
+    if (a[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Arount of positive numbers : {sum}");
